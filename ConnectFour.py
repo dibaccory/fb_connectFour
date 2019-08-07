05ef6baa1db7fafb89516ff5f52e8310799f79a0
Richard <thedibaccle@gmail.com>
2019-08-07 12:04:54 -0700

    small fixes

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,11 +1,44 @@
 import sys
+
+def opponent_color_of(blue):
+    return 'y' if blue == 'r' else 'r'
+
+def discs_dropped(game_state):
+    return len(''.join(str(disc) if disc else '' for row in game_state for disc in row))
+
+def has_winner_helper(game_state):
+    #horizontal win
+    for r in range(5, -1, -1):
+        for c in range(4):
+            if((game_state[r][c] == game_state[r][c+1] == game_state[r][c+2] == game_state[r][c+3] == 'r')
+            or (game_state[r][c] == game_state[r][c+1] == game_state[r][c+2] == game_state[r][c+3] == 'y')):
+                return True;
+    #vertical win
+    for c in range(7):
+        for r in range(2, -1, -1):
+            if((game_state[r][c] == game_state[r+1][c] == game_state[r+2][c] == game_state[r+3][c] == 'r')
+            or (game_state[r][c] == game_state[r+1][c] == game_state[r+2][c] == game_state[r+3][c] == 'y')):
+                return True;
+    #diagonal wins
+    for r in range(2, -1, -1):
+        for c in range(4): #left-to-right
+            if((game_state[r][c] == game_state[r+1][c+1] == game_state[r+2][c+2] == game_state[r+3][c+3] == 'r')
+            or (game_state[r][c] == game_state[r+1][c+1] == game_state[r+2][c+2] == game_state[r+3][c+3] == 'y')):
+                return True
+
+        for c in range(3, 7): #right-to-left
+            if((game_state[r][c] == game_state[r+1][c-1] == game_state[r+2][c-2] == game_state[r+3][c-3] == 'r')
+            or (game_state[r][c] == game_state[r+1][c-1] == game_state[r+2][c-2] == game_state[r+3][c-3] == 'y')):
+                return True
+    return False
+
 #Ensure that each column is properly stacked
 #Only None, 'r', 'y' are acceptable entries
 def is_state_valid(game_state):
     for c in range(7):
         top_col = False
         for r in range(6):
-            if game_state[r][c] == "r" or game_state[r][c] == "y":
+            if game_state[r][c] == 'r' or game_state[r][c] == 'y':
                 top_col = True
             elif game_state[r][c]: #something other than 'r' or 'y'
                 return False
@@ -13,57 +46,35 @@ def is_state_valid(game_state):
                 return False
     return True
 
-def opponent_color_of(blue):
-    return "y" if blue == "r" else "r"
-
-def discs_dropped(game_state):
-    return len(''.join(str(disc) if disc else '' for row in game_state for disc in row))
-
+#If total amount of discs in the game is divisble by two, then it's the first player's turn.
 def get_current_player(game_state):
-    if is_state_valid(game_state):
+    global integrity_asserted
+    if integrity_asserted:
+        return opponent_color_of(first_player) if discs_dropped(game_state)%2 else first_player
+    elif is_state_valid(game_state):
         return opponent_color_of(first_player) if discs_dropped(game_state)%2 else first_player
     else:
-        print(msg[0]) #invalid state
+        return msg[0] #invalid state
 
 def has_winner(game_state):
-    if is_state_valid(game_state): #Do you really win if the state isn't valid? (no)
-        #horizontal win
-        for r in range(5, 0, -1):
-            for c in range(4):
-                if((game_state[r][c] == game_state[r][c+1] == game_state[r][c+2] == game_state[r][c+3] == "r")
-                or (game_state[r][c] == game_state[r][c+1] == game_state[r][c+2] == game_state[r][c+3] == "y")):
-                    return True;
-
-        #vertical win
-        for c in range(7):
-            for r in range(2, 0, -1):
-                if((game_state[r][c] == game_state[r+1][c] == game_state[r+2][c] == game_state[r+3][c] == "r")
-                or (game_state[r][c] == game_state[r+1][c] == game_state[r+2][c] == game_state[r+3][c] == "y")):
-                    return True;
-
-        #diagonal wins
-        for r in range(2, 0, -1):
-            for c in range(4): #left-to-right
-                if((game_state[r][c] == game_state[r+1][c+1] == game_state[r+2][c+2] == game_state[r+3][c+3] == "r")
-                or (game_state[r][c] == game_state[r+1][c+1] == game_state[r+2][c+3] == game_state[r+3][c+3] == "y")):
-                    return True
-
-            for c in range(3, 7): #right-to-left
-                if((game_state[r][c] == game_state[r+1][c-1] == game_state[r+2][c-2] == game_state[r+3][c-3] == "r")
-                or (game_state[r][c] == game_state[r+1][c-1] == game_state[r+2][c-2] == game_state[r+3][c-3] == "y")):
-                    return True
+    global integrity_asserted
+    if integrity_asserted:
+        return has_winner_helper(game_state)
+    elif is_state_valid(game_state): #Do you really win if the state isn't valid? (no)
+        return has_winner_helper(game_state)
     else:
-        print(msg[0]) #invalid state
-        return
+        return msg[0] #invalid state
 
     return False
 
 def play(game_state, column, color):
-    if is_state_valid(game_state): #make sure it's the player's turn before they're able to move
-        if get_current_player(game_state) == color:
-            if has_winner(game_state):
-                print(msg[3]) #Already a winner on board.
-                return
+    global integrity_asserted
+    if is_state_valid(game_state):
+        integrity_asserted = True
+        if has_winner(game_state):
+            print(msg[3]) #Already a winner on board.
+            return
+        if color == get_current_player(game_state): #make sure it's the player's turn before they're able to move
             r=5
             while r>=0:
                 if not game_state[r][column]:
@@ -71,19 +82,21 @@ def play(game_state, column, color):
                 r -= 1
             if r>=0: #check if column full
                  game_state[r][column] = color;
-                 if has_winner(game_state):
-                     print(msg[1], *game_state, sep='\n') #winner!
-                     return
+                 return game_state
             else:
-                print(msg[6]) #Column full
+                return msg[6] #Column full
+        elif color in ['y','r']:
+            return msg[7] #not players' turn
         else:
-            print(msg[7]) #not players' turn
+            return msg[8] #invalid color
     else:
-        print(msg[0]) #invalid state
-    return game_state
+        return msg[0] #invalid state
+
+
 
 game_state = [[None] * 7 for _ in range(6)]
-first_player = "y"
+first_player = 'y'
+global integrity_asserted# = False
 msg = [
 "ERROR: Invalid game state.",
 "We have a winner!",
@@ -93,10 +106,11 @@ msg = [
 "ERROR: Number out of bounds. Try again.",
 "ERROR: Chosen column is full. Select a different column index.",
 "ERROR: Not this player's turn.",
+"ERROR: Invalid color.",
 ]
 
-
 if __name__ == '__main__':
+    global integrity_asserted
     moves_left = 42 #instead of calling discs_dropped 42 times
     while game_state: #while condition can be replaced with len(moves_made) <42
         if not moves_left:
@@ -114,7 +128,18 @@ if __name__ == '__main__':
                 print(msg[4])
             except IndexError:
                 print(msg[5])
-        game_state = play(game_state, col, get_current_player(game_state))
-        moves_left -= 1
+
+        integrity_asserted = False
+        res = play(game_state, col, get_current_player(game_state))
+        if type(res) is str:
+            print(res)
+        elif not integrity_asserted:
+            break
+        else:
+            moves_left -= 1
+            
+        if has_winner(game_state):
+            print(msg[1], *game_state, sep='\n') #winner!
+            break
     print("Ending game...")
     sys.exit()
